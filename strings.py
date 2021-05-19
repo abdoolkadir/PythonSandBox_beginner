e6a7f9a62dbcf868414d02126149440ccf6311cc
Abdulkadir Adamu <adamuhammadabdulkadir@gmail.com>
2021-05-19 14:42:43 +0100

    Initial Commit for all py files

---
+++ strings.py
@@ -0,0 +1,21 @@
+# Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
+
+name = 'Abdul'
+age = 24
+
+# Concatenate
+# print('Hello, my name is ' + name + ' and i am ' + str(age))
+
+# String Formatting
+
+# Arguments by Position
+# print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+# F-Strings 
+# print(f'Hello my name is {name} and i am {age}')
+
+# String Methods
+s = 'Hello world'
+
+# capitalize string
+print(s.capitalize())
